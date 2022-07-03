e87d34463a5f2e7b76d62769969f60d07f31b1e4
sahksas <2conplayer@gmail.com>
2022-07-03 21:28:59 +0900

    update utils.ts

--- src/utils.ts
+++ src/utils.ts
@@ -1,17 +1,38 @@
-export const waitPageLoad = (selector: string, delay = 500, maxCheck = 15) =>
-  new Promise((resolve) => {
-    /**
-     *
-     */
+export const waitSelectorLoad = (selector: string, delay = 500, maxCheck = 15) =>
+  new Promise((resolve, reject) => {
     let checkCount = 0;
     const elmCheck = () => {
       checkCount++;
       const elms = document.querySelectorAll(selector);
-      if (checkCount > maxCheck || elms.length > 0) {
+      if (elms.length > 0) {
         resolve(elms);
       } else {
         // console.log(elms)
-        setTimeout(elmCheck, delay);
+        if (checkCount <= maxCheck) {
+          setTimeout(elmCheck, delay);
+        } else {
+          reject("Elm is nothing");
+        }
+      }
+    };
+    elmCheck();
+  });
+
+export const waitClassNameLoad = (className: string, delay = 500, maxCheck = 15) =>
+  new Promise((resolve, reject) => {
+    let checkCount = 0;
+    const elmCheck = () => {
+      checkCount++;
+      const elms = document.getElementsByClassName(className);
+      if (elms.length > 0) {
+        resolve(elms);
+      } else {
+        // console.log(elms)
+        if (checkCount <= maxCheck) {
+          setTimeout(elmCheck, delay);
+        } else {
+          reject("Elm is nothing");
+        }
       }
     };
     elmCheck();
