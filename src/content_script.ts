60e123c141b3e27c471367f618adc4e8496fbd67
sahksas <2conplayer@gmail.com>
2022-07-09 15:55:46 +0900

    remove domManipulation

--- src/content_script.ts
+++ src/content_script.ts
@@ -2,23 +2,13 @@ import { waitSelectorLoad } from "./utils";
 import * as consts from "./constants";
 import * as styleChanges from "./style_changes";
 
-function domManipulation() {
-  waitSelectorLoad(consts.OVERLAY_INNER_PATH)
-    .then(() => {
-      styleChanges.styleChange();
-    })
-    .catch(() => {
-      // console.log("err");
-    });
-}
-
 waitSelectorLoad(consts.OVERLAY_PARENT_PATH).then(() => {
   const target = document.querySelector(consts.OVERLAY_PARENT_PATH)! as HTMLElement;
   const observer = new MutationObserver((mutations) => {
     mutations.forEach(() => {
       const overlayInner = target.querySelector("div > div.notion-peek-renderer") as HTMLElement;
       if (overlayInner) {
-        domManipulation();
+        styleChanges.styleChange();
       } else {
         styleChanges.undoStyleChange();
       }
